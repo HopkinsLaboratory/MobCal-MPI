fd63fcb57e9ecabf2634afe927bf263a0d310fc6
ChristianIeritano <44277344+ChristianIeritano@users.noreply.github.com>
2023-10-27 10:56:30 -0400

    Add files via upload

---
+++ ifort_gfortran_comparison/SMT_Unix/runmob_OpenMPI_Unix.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+ ls *.mfj > mobfit.in 
+ input="mobfit.in"
+ suffix=".mfj"
+ cnt=0
+ one=1
+ while IFS= read -r var
+ do
+  inp=${var%$suffix}
+  echo "$inp" > temp.in
+  tail -n +2 $var >> temp.in
+  cp temp.in $var
+  rm temp.in
+  echo "$inp.mfj" > mobcal.run
+  echo "$inp.mout" >> mobcal.run
+  mobinp="mobcal.run_$cnt"
+  mv mobcal.run $mobinp
+  echo "running $inp.mfj"
+  mpirun -np 4 ./MobCal_MPI2.exe $mobinp &
+  sleep 0.5s
+  cnt=$(($cnt+$one))
+ done < "$input"
+ rm $input
