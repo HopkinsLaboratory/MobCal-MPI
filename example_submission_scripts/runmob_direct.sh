2af2ec7ba71ad4e866ef0a2202fbe308ecc19f5c
ChristianIeritano <cieritan@uwaterloo.ca>
2024-05-13 20:33:30 -0400

    MobCal-MPI 2.0.3 release
    upgraded the submission process for better compatibility with command line interfaces: file names for input and output are now plucked directly from command line arguments, not from an external file.
    
    The manual and example submission scripts have been updated to reflect these changes

--- example_submission_scripts/runmob_direct.sh
+++ example_submission_scripts/runmob_direct.sh
@@ -2,22 +2,11 @@
  ls *.mfj > mobfit.in 
  input="mobfit.in"
  suffix=".mfj"
- cnt=0
- one=1
  while IFS= read -r var
  do
   inp=${var%$suffix}
-  echo "$inp" > temp.in
-  tail -n +2 $var >> temp.in
-  cp temp.in $var
-  rm temp.in
-  echo "$inp.mfj" > mobcal.run
-  echo "$inp.mout" >> mobcal.run
-  mobinp="mobcal.run_$cnt"
-  mv mobcal.run $mobinp
   echo "running $inp.mfj"
-  mpirun -np 4 ./MobCal_MPI_202.exe $mobinp &
+  mpirun -np 4 ./MobCal_MPI_203.exe "$inp.mfj" "$inp.mout" &
   sleep 0.5s
-  cnt=$(($cnt+$one))
  done < "$input"
  rm $input
